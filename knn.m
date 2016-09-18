0f451ed8efb8ed66ffa526ff0c48cf14a47db3e1
J. Knestaut <kjosh9@vt.edu>
2016-09-18 15:07:07 -0400

    made comments for steps

--- knn.m
+++ knn.m
@@ -1,24 +1,23 @@
 
-%loading images
-
-
-clear param
-img1 = imread('demo1.jpg');
-
-% Parameters:
-clear param
-param.imageSize = [256 256]; % it works also with non-square images
-param.orientationsPerScale = [8 8 8 8];
-param.numberBlocks = 4;
-param.fc_prefilt = 4;
-
-% Computing gist requires 1) prefilter image, 2) filter image and collect
-% output energies
-[gist1, param] = LMgist(img1, '', param);
 
 
 function predlabels = knn(traindata, trainlabels, testdata, k, f, D)
     
+    %calculate the dissimilarity of the test point x onto each of the train points
+    l = size(,2);
+    r = 1:l;
+    D = pdist2(X(:,r),transpose(X(r,:))).;
+    
+    %find the train point x^n wich is nearest to x
+    %n = argmin(D);
+    
+    %assign the class label
+    
+    
+    %in the case that there are two or more nearest neighbours with
+    %different class labels, the most numerous class is chosen.
+    
+
 
 end
 
