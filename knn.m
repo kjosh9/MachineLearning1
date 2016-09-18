e71bece0c357e961c2beeff2efd67a587105dc2b
J. Knestaut <kjosh9@vt.edu>
2016-09-18 17:23:29 -0400

    making some progress

--- knn.m
+++ knn.m
@@ -1,22 +1,51 @@
 
-
+%testfeatgist - testdata
+%trainlabels - trainlabels
+%trainfeatgist - traindata
+%k - number of nearest neighbors
+%f - distance metric
+%D - ?
 
 function predlabels = knn(traindata, trainlabels, testdata, k, f, D)
     
-    %calculate the dissimilarity of the test point x onto each of the train points
-    l = size(,2);
-    r = 1:l;
-    D = pdist2(X(:,r),transpose(X(r,:))).;
+
+    %calculate the dissimilarity of the test point x onto each of the train points    
+    l1 = size(testdata, 1);
+    l1 = 1;
+    predlabels = zeros(l1,1);
+    l2 = size(traindata, 1);
     
-    %find the train point x^n wich is nearest to x
-    %n = argmin(D);
-    
-    %assign the class label
-    
+    for i = 1:l1
+        
+        for i2 = 1:l2
+            Dist = pdist2(testdata(i,:), traindata, 'euclidean');
+        end
+        
+        %find the train point x^n wich is nearest to x
+        
+        %sort the stuff to be in an order of least to greatest
+        
+        
+        kindex = zeros(k,3);
+        for i3 = 1:k
+            %find the i3th closest index
+            %put in kindex
+                       
+            [kindex(k,1),kindex(k,2)] = min(Dist);
+            kindex(k,3) = trainlabels(kindex(k,2),1)
+        end
+        
+        %assign the class label
+        %find the most common class of kindex and assign to predlabels(i)
+        predlabels = mode(kindex(:,3))
+        
+        
+        
+    end
     
     %in the case that there are two or more nearest neighbours with
     %different class labels, the most numerous class is chosen.
-    
+    %maybe not instantiate this
 
 
 end
