568b61e3a54c8d09bef80da1b791600052d86006
J. Knestaut <kjosh9@vt.edu>
2016-09-18 18:14:34 -0400

    initial cross-validation commit

---
+++ knncv.m
@@ -0,0 +1,13 @@
+%
+
+
+
+function cv_error = knncv(traindata, trainlabels, n, k, f, D)
+    
+    labels = reshape(trainlabels, n, []);
+
+    
+    %predlabels = knn()
+    
+    cv_error = 1;
+end
