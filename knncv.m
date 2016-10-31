8e27f8ed2aed1ebcf4faac1363dfa9096443b152
kjosh9 <kjosh9@vt.edu>
2016-10-31 00:58:30 -0400

    playing around with another class for crossval

--- knncv.m
+++ knncv.m
@@ -1,8 +1,11 @@
 
 %https://www.mathworks.com/help/stats/cvpartition.html
+%https://www.mathworks.com/help/bioinfo/ref/crossvalind.html
 
 function cv_error = knncv(traindata, trainlabels, n, k, f, D)
     
-    c = cvpartition(traindata, 'HoldOut', n);
-        
+    %c = cvpartition(traindata, 'HoldOut', n);
+    [Train, Test] = crossvalind('HoldOut', size(traindata,1), P);   
+    
+    
 end
